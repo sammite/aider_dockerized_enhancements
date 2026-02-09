4a24c334f097b05567942079db744a461f05d779
Your Name <you@example.com>
2026-02-08 19:24:28 -0500

    made things more generic so people can actually use this stuff

--- run_aider.sh
+++ run_aider.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-#SYSTEM_PROMPT="
-#<instructions>
-#\"Do not edit the files directly. Instead, write the delivered .patch file contents
-#to swap_diff.patch and then run the following command to merge the differences.
-#patch <FILE> swap_diff.patch
-#Then, zero out the swap_diff.patch file.\"
-#</instructions>
-#"
+[ -f .env ] && source .env
 
 sudo docker run -it \
     --user $(id -u):$(id -g) \
     --network host \
     --volume $(pwd):/app \
     --env DISPLAY=$DISPLAY \
-    --env OLLAMA_API_BASE=http://192.168.56.1:11434 \
+    --env OLLAMA_API_BASE=http://$MODEL_HOST_IP:11434 \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     aider-xclip \
     --no-check-update \
-    --model ollama_chat/qwen2.5-coder:3b
+    --model ollama_chat/$MODEL_SLUG
 #   --copy-paste 
