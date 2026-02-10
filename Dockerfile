4b20c0952503ee2c158463ca5d3df5b82d298d34
sammite <54330632+sammite@users.noreply.github.com>
2026-02-10 02:42:08 +0000

    build: apply patch and update script

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,16 @@
-FROM paulgauthier/aider-full
+FROM paulgauthier/aider-full@sha256:271fb5285a21591f1a43e13674d7783d34593f4f17e7eb5049d9e5245f6fb340
 
 USER root
 
-RUN apt-get update && apt-get install -y \
-    xclip vim
+RUN apt-get update && apt-get install --no-install-recommends -y \
+    xclip vim && \
+    rm -rf /var/lib/apt/lists/*
 
-USER aider
+USER appuser
 
 WORKDIR /app
+
+COPY commands.py.patch .
+
+RUN patch /venv/lib/python3.10/site-packages/aider/commands.py commands.py.patch && \
+    rm commands.py.patch
--- run_aider.sh
+++ run_aider.sh
@@ -7,13 +7,14 @@ if ! docker ps >/dev/null 2>&1; then
     IS_SUDO="sudo"
 fi
 
-$IS_SUDO docker run -it \
-    --user $(id -u):$(id -g) \
-    --network host \
-    --volume $(pwd):/app \
-    --env DISPLAY=$DISPLAY \
-    --env OLLAMA_API_BASE=http://$MODEL_HOST_IP:11434 \
-    --volume /tmp/.X11-unix:/tmp/.X11-unix \
+$IS_SUDO docker run --rm -it \
+    --user "$(id -u):$(id -g)" \
+    --security-opt=no-new-privileges \
+    --workdir /app \
+    --volume "$(pwd):/app" \
+    --env DISPLAY="$DISPLAY" \
+    --env OLLAMA_API_BASE="http://$MODEL_HOST_IP:11434" \
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
     aider-xclip \
     --no-check-update \
-    --model ollama_chat/$MODEL_SLUG
+    --model "ollama_chat/$MODEL_SLUG"
